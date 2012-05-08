4f36ddb4f4761ec9a4183dcd323131a0d16e31d2
moandji.ezana <mwanji@gmail.com>
2012-05-08 23:52:34 +0200

    Added javadoc

--- src/main/java/com/moandjiezana/lifegycle/jx/LifegycleJxModules.java
+++ src/main/java/com/moandjiezana/lifegycle/jx/LifegycleJxModules.java
@@ -8,13 +8,29 @@ import org.nnsoft.guice.lifegycle.DisposeModule;
 
 import com.google.inject.matcher.Matchers;
 
+/**
+ * A factory to create Lifegycle modules configured to use standard annotations from the javax.annotations package
+ * 
+ * @author Moandji Ezana
+ * @since 0.1
+ */
 public class LifegycleJxModules {
 
+  /**
+   * @return An {@link AfterInjectionModule} that uses the {@link PostConstruct} annotation to identify handlers.
+   * @since 0.1
+   */
   public static AfterInjectionModule afterInjectionModule() {
     return new AfterInjectionModule(PostConstruct.class, Matchers.any());
   }
 
+  /**
+   * @return A {@link DisposeModule} that uses a {@link PreDestroy} annotation to identify handlers.
+   * @since 0.1
+   */
   public static DisposeModule disposeModule() {
     return new DisposeModule(PreDestroy.class, Matchers.any());
   }
+  
+  private LifegycleJxModules() {}
 }
